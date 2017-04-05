292dfec643942a690808685140bc54d102c1113a
JazzType <akash.kulhalli@gmail.com>
2017-04-05 19:25:24 +0530

    Finalized PHP files

--- login_page.php
+++ login_page.php
@@ -1,13 +1,12 @@
 <?php 
 session_start();
+include_once('template.php');
+if(isset($_SESSION['userid'])) {
+	header('Location: index.php');
+}
+
 ?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Login - Nomster.com</title>
-		<link rel="stylesheet" type="text/css" href="css/login.css">
-	</head>
-	<body>		
+<body>		
 		<script type="text/javascript">
 			function attempt() {
 				var email = document.getElementById('emailID');
@@ -21,11 +20,12 @@ session_start();
 					xmlhttp.onreadystatechange = function() {
 						if(this.readyState == 4 && this.status == 200) {
 							if(this.responseText == "true") {
-								window.location = 'form_page.php?email=' + email.value;
+								window.location = 'index.php';
 							}
 							else {
+								console.log(this.responseText);
 								var errordiv = document.getElementById('errorlabel');
-								errordiv.innerHTML = 'Address in use. Please try again';
+								errordiv.innerHTML = 'Incorrect credentials';
 								errordiv.style.backgroundColor = '#ff6666';
 							}
 						}
@@ -34,23 +34,34 @@ session_start();
 				}
 			}
 		</script>
-		<div id="header">
-			<img src="favicon.ico" width="24px" height="24px">
-			Nomster.com
+		<div class="container">
+			<div class="row text-center">Login to continue<hr><br></div>
+			<div class="row text-center">
+			<div class="col-sm-4"></div>
+				<div class="col-sm-4" style="background-color: #f1f2f1;vertical-align: middle;box-shadow: 0 0 15px rgba(0,0,0,0.15), 0 1px 3px rgba(0,0,0,0.15); padding-top: 10px;padding-bottom: 10px;">
+						
+					<div class="row">
+						<label for="emailID">Email address</label><br>
+						<input type="email" name="emailID" id="emailID" required placeholder="someone@example.com" autofocus class="input-lg">						
+					</div><br>
+					<div class="row">
+						<label for="password">Password</label><br>
+						<input type="password" name="password" id="password" required placeholder="Password" class="input-lg">
+					</div><br>
+					<div class="row">
+						<div id="errorlabel" class="text-danger text-center"></div>						
+					</div>
+					<div class="row">
+						<div class="col-4-1"></div>
+						<div class="col-4-3">
+							<input type="submit" name="submit" id="submitbtn" onclick="attempt()" class="btn btn-success">
+							<input type="reset" name="reset" id="resetbtn" class="btn">
+						</div>
+					</div>	
+					<hr>
+					<button class="btn btn-block btn-info" onclick="window.location='register.php'">New User? Click here!</button>				
+				</div>
 			</div>
-		<div id="pageTitle">
-			Login to your profile
-		</div>
-		<div id="pagediv">
-			
-			<label for="emailID">Registered E-Mail address</label><br>
-				<input type="email" name="emailID" id="emailID" required placeholder="someone@example.com" autofocus><br>
-				<label for="password">Password</label><br>
-				<input type="password" name="password" id="password" required placeholder="Password">
-				<div id="errorlabel"></div><br>
-				<input type="submit" name="submit" id="submitbtn" onclick="attempt()">
-				<input type="reset" name="reset" id="resetbtn">
-			
-		</div>
+		</div>				
 	</body>
 </html>
